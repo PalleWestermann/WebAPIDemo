1db98aecc581341071739ec923d808cdaa7e89ee
Palle Westermann <pwe@tec.dk>
2025-10-01 10:21:56 +0200

    Raw Controller Class without routes

---
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemo.Controllers
+{
+    // Explanation: Now, 3 things make this class a Web API Controller.
+    // 1. [ApiController]
+    // 2. the class must be Deriving from ControllerBase
+    // 3. and naming the clas <something>Controller
+
+    [ApiController]
+    public class ShirtsController : ControllerBase
+    {
+        // https://localhost:7134/shirts
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
+
+        public string GetSHirtById(int id) 
+        {
+            return $"Reading shirt: {id}";
+        }
+
+        public string CreateShirt()
+        {
+            return $"Creating a shirt";
+        }
+
+        public string UpdateShirt(int id)
+        {
+            return $"Updating shirt: {id}";
+        }
+
+        public string DeleteShirt(int id)
+        {
+            return $"Deleting shirt: {id}";
+        }
+    }
+}
